92fe69f17ff4093fa2668c2b042c6c3b559045f9
Jeff Smits <jeff.smits@gmail.com>
2017-06-22 13:58:02 +0200

    Added examples of parse/analyze/transform for string input

--- org.metaborg.example.api.transformation/src/main/java/org/metaborg/example/api/transformation/Builder.java
+++ org.metaborg.example.api.transformation/src/main/java/org/metaborg/example/api/transformation/Builder.java
@@ -1,21 +1,33 @@
 package org.metaborg.example.api.transformation;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.VFS;
 import org.metaborg.core.MetaborgException;
 import org.metaborg.core.action.EndNamedGoal;
+import org.metaborg.core.analysis.AnalysisException;
 import org.metaborg.core.build.BuildInput;
 import org.metaborg.core.build.BuildInputBuilder;
+import org.metaborg.core.context.IContext;
 import org.metaborg.core.language.ILanguageImpl;
 import org.metaborg.core.project.IProject;
+import org.metaborg.core.syntax.ParseException;
 import org.metaborg.core.transform.TransformException;
 import org.metaborg.spoofax.core.Spoofax;
 import org.metaborg.spoofax.core.build.ISpoofaxBuildOutput;
 import org.metaborg.spoofax.core.resource.SpoofaxIgnoresSelector;
+import org.metaborg.spoofax.core.unit.ISpoofaxAnalyzeUnit;
+import org.metaborg.spoofax.core.unit.ISpoofaxInputUnit;
+import org.metaborg.spoofax.core.unit.ISpoofaxParseUnit;
 import org.metaborg.spoofax.core.unit.ISpoofaxTransformUnit;
+import org.metaborg.util.concurrent.IClosableLock;
 import org.spoofax.interpreter.terms.IStrategoInt;
 import org.spoofax.interpreter.terms.IStrategoTerm;
+
+import com.google.common.io.Files;
 
 public class Builder {
 
@@ -76,6 +88,33 @@ public class Builder {
 			throw new TransformException("Coul not evaluate");
 
 		IStrategoTerm result = output.transformResults().iterator().next().ast();
+		
+		if (result instanceof IStrategoInt)
+			return ((IStrategoInt) result).intValue();
+		else
+			throw new TransformException("Result is not an integer");
+	}
+	
+	public int evaluate(String content) throws MetaborgException, IOException, InterruptedException {
+		FileObject file = VFS.getManager().toFileObject(File.createTempFile("temp", "example", Files.createTempDir()));
+		ISpoofaxInputUnit input = spoofax.unitService.inputUnit(file, content, implementation, null);
+		ISpoofaxParseUnit parsed = spoofax.syntaxService.parse(input);
+		if (!parsed.valid())
+			throw new ParseException(input, "Could not parse");
+
+		IContext context = SpoofaxUtil.getContext(spoofax, implementation, file.getParent());
+		ISpoofaxAnalyzeUnit analyzed;
+		try (IClosableLock lock = context.write()) {
+			analyzed = spoofax.analysisService.analyze(parsed, context).result();
+			if (!analyzed.valid())
+				throw new AnalysisException(context, "Could not analyse");
+		}
+		Collection<ISpoofaxTransformUnit<ISpoofaxAnalyzeUnit>> transformUnits;
+		try(IClosableLock lock = context.read()) {
+			transformUnits = spoofax.transformService.transform(analyzed, context, new EndNamedGoal("Evaluate"));
+		}
+
+		IStrategoTerm result = transformUnits.iterator().next().ast();
 		
 		if (result instanceof IStrategoInt)
 			return ((IStrategoInt) result).intValue();
